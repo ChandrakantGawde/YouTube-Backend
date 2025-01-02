d17f435837731b89e8c541e28e632636678d97a6
Chandrakant Gawde <chandrakantgawde3@gmail.com>
2025-01-02 23:35:19 +0530

    BugFix:DataBaseName

--- src/db/index.js
+++ src/db/index.js
@@ -6,7 +6,7 @@ const connectDB = async ()=> {
         const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`)
     } catch(error){
-       console.log("MONGODB connection error ", error); 
+       console.log("MONGODB connection failed ", error); 
        process.exit(1); // node.js provide extra feature called process to get out of this like exsit 
     }
 }
